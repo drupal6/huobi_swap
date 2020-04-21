47d8e985ad885d890abbc99e97263c3a8566831f
605355232@qq.com <dreams1234>
2020-04-21 20:51:11 +0800

    完善网格

--- strategy/quantification_strategy.py
+++ strategy/quantification_strategy.py
@@ -20,12 +20,14 @@ class QuantificationStrategy(BaseStrategy):
     def __init__(self):
         self.price_margin = []  # 设置网格价格
         self.position_weight = []  # 设置网格的仓位
+        self.position_weight_label = []  # 设置网格仓位标签
         self.band = None  # 网格价格
         self.atr = 0  # 真实波幅
         self.atr_per = 0.05   # 最小网格高度要求
         self.min_index = -1  # 网格基准先位置
         self.grids = deque(maxlen=10)  # 记录价格在网格中的位置
         self.close_position_rate = 5  # 平仓价格倍数
+        self.margin_num_limit = 4  # 最少网格要求
         super(QuantificationStrategy, self).__init__()
 
     def reset_bank(self, df):
@@ -38,6 +40,7 @@ class QuantificationStrategy(BaseStrategy):
         #     if len(open_orders) == 0 and position.short_quantity == 0 and position.long_quantity == 0:
         #         reset = True
         if reset:
+            self.atr = 0
             df["atr"] = talib.ATR(df["high"], df["low"], df["close"], 20)
             df["max_high"] = talib.MAX(df["high"], self.klines_max_size)
             df["min_low"] = talib.MIN(df["low"], self.klines_max_size)
@@ -45,29 +48,55 @@ class QuantificationStrategy(BaseStrategy):
             atr = current_bar["atr"]
             if current_bar["close"] * self.atr_per / self.lever_rate > atr:  # 网格高度达不到最小要求
                 return
+            num = math.floor((current_bar["max_high"] - current_bar["min_low"])/atr)
+            if num < self.margin_num_limit:  # 网格太少
+                return
             self.atr = atr
-            num = math.floor((current_bar["max_high"] - current_bar["min_low"])/self.atr)
             if num % 2 == 0:
                 num = num + 1
             self.min_index = math.floor(num/2)
             self.grids.clear()
             self.price_margin = []
             self.position_weight = []
+            self.position_weight_label = []
+            self.price_margin.append(round_to((-(self.min_index + self.close_position_rate) * self.atr), self.price_tick))
             for i in range(0, num):
                 self.price_margin.append(round_to((i - self.min_index) * self.atr, self.price_tick))
-                self.position_weight.append(1)
+            self.price_margin.append(round_to(((self.min_index + self.close_position_rate) * self.atr), self.price_tick))
             # df['olhc'] = df[["open", "close", "high", "low"]].mean(axis=1)
-            self.band = np.mean(df['close']) + np.array(self.price_margin) * np.std(df['close'])
-            print(self.price_margin)
-            print(self.position_weight)
-            print(self.band)
-            # 计算各个网格的价格
+            self.band = np.mean(df['close']) + np.array(self.price_margin) * np.std(df['close']) # 计算各个网格的价格
+            # 计算仓位
             if self.trading_curb == "long":
-                pass
+                for i in range(0, num):
+                    if i == 0:
+                        self.position_weight.append(num - 1)
+                    if i == num - 1:
+                        self.position_weight.append(0)
+                    else:
+                        self.position_weight.append(num - i - 1)
+                    self.position_weight_label.append(i)
             elif self.trading_curb == "short":
-                pass
+                for i in range(0, num):
+                    if i == 0:
+                        self.position_weight.append(0)
+                    if i == num - 1:
+                        self.position_weight.append(num - 1)
+                    else:
+                        self.position_weight.append(i + 1)
+                    self.position_weight_label.append(i)
             else:
-                pass
+                for i in range(0, num):
+                    if i == self.min_index:
+                        self.position_weight.append(0)
+                    index = i
+                    if i == 0:
+                        index = 1
+                    elif i == num - 1:
+                        index = num - 2
+                    self.position_weight.append(abs(self.min_index - index))
+                    self.position_weight_label.append(i)
+
+            self.position_weight_label.append(num)
 
     def calculate_signal(self):
         self.long_status = 0
@@ -80,50 +109,20 @@ class QuantificationStrategy(BaseStrategy):
         if self.atr == 0:
             return
         current_bar = df.iloc[-1]
-        band_size = len(self.band)
-        grid = -3
-        for i in range(0, band_size):
-            if i == 0:
-                if current_bar["close"] <= self.band[i] - self.atr * self.close_position_rate:
-                    grid = -2
-                    break
-                if (self.band[i] - self.atr * self.close_position_rate) < current_bar["close"] <= self.band[i]:
-                    grid = i
-                    break
-            if i == band_size -1:
-                if current_bar["close"] >= self.band[i] + self.atr * self.close_position_rate:
-                    grid = -1
-                    break
-                if self.band[i] <= current_bar["close"] < (self.band[i] + self.atr * self.close_position_rate) :
-                    grid = i
-                    break
-            if i < self.min_index:
-                if (self.band[i] - self.atr * self.close_position_rate) < current_bar["close"] <= self.band[i]:
-                    grid = i
-                    break
-            if i > self.min_index:
-                if self.band[i] <= current_bar["close"] < (self.band[i] + self.atr * self.close_position_rate):
-                    grid = i
-                    break
+        grid = -1
+        if current_bar["close"] <= self.band[0]:
+            grid = -2
+        elif current_bar["close"] >= self.band[-1]:
+            grid = -3
+        else:
+            grid = pd.cut([current_bar["close"]], self.band, labels=self.position_weight_label)[0]
 
-        # grid = pd.cut([current_bar["close"]], self.band, labels=self.position_weight_label)[0]
-        if len(self.grids) == 0:
-            self.grids.append(grid)
-        if self.grids[-1] != grid:
-            self.grids.append(grid)
-        # if len(self.grids) == 1:
+        print(self.price_margin)
+        print(self.position_weight)
+        print(self.position_weight_label)
+        print(self.band)
+        print(current_bar["close"], grid)
 
-        # if grid == pos:
-        #     self.long_status = -1
-        #     self.short_status = -1
-        # if grid < pos - 1:
-        #     self.long_status = 1
-        #     self.short_status = -1
-        #     self.long_trade_size = self.position_weight[grid]
-        # if grid > pos + 1:
-        #     self.long_status = -1
-        #     self.short_status = 1
-        #     self.short_trade_size = self.position_weight[grid]
 
 
 
