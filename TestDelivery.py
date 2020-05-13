a695b6642f15422c46c30fe911f439da60ea1480
605355232@qq.com <dreams1234>
2020-05-13 17:43:44 +0800

    加图例

--- TestDelivery.py
+++ TestDelivery.py
@@ -2,19 +2,17 @@ from api.huobi.huobi_request_swap import HuobiSwapRequest
 import asyncio
 
 if __name__ == '__main__':
-    p = {"lever_rate": 50}
-    print(p.get("lever_rate", 20))
-    # request = HuobiSwapRequest("https://api.btcgateway.pro", "xxxx", "xxxx")
-    #
-    # async def get_data():
-    #     success, error = await request.create_order(contract_code="BTC-USD", price=10000, quantity=1, direction="sell",
-    #                                                 offset="open", lever_rate=50, order_price_type="limit")
-    #     print(success)
-    #     print(error)
-    #
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(get_data())
-    # loop.close()
+    request = HuobiSwapRequest("https://api.btcgateway.pro", "xxxx", "xxxxx")
+
+    async def get_data():
+        success, error = await request.create_order(contract_code="BTC-USD", price=8851.6, quantity=1, direction="buy",
+                                                    offset="open", lever_rate=50, order_price_type="limit")
+        print(success)
+        print(error)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_data())
+    loop.close()
 
 
 
---
+++ matploat/kline_pic1.py
@@ -0,0 +1,58 @@
+from api.huobi.huobi_request_swap import HuobiSwapRequest
+import asyncio
+import pandas as pd
+import mplfinance as mpf
+import matplotlib as mpl
+from cycler import cycler
+from matplotlib import pyplot as plt
+import talib
+mpl.use('TkAgg')
+
+
+class MatPlot:
+
+    @classmethod
+    async def get_data(cls, symbol, period="1min", size=200):
+        success, error = await request.get_klines(contract_type=symbol, period=period, size=size)
+        if error:
+            return None
+        if success:
+            data = success.get("data")
+            df = pd.DataFrame(data, columns={"id": 0, 'vol': 1, 'count': 2, 'open': 3, 'close': 4, 'low': 5,
+                                                       'high': 6, 'amount': 7})
+            df = df[['id', 'open', 'high', 'low', 'close', 'vol']]
+            df = df.rename(
+                columns={"id": "Date", "open": "Open", "high": "High", "low": "Low", "close": "Close", "vol": "Volume"})
+            df["Date"] = pd.to_datetime(df["Date"], unit="s")
+            df.set_index(["Date"], inplace=True)
+            MatPlot.show(df, symbol, period, size)
+
+    @classmethod
+    def show(cls, df, symbol, period="1min", size=200):
+        df['fast_ema'] = talib.EMA(df['Close'], timeperiod=5)
+        df['slow_ema'] = talib.EMA(df['Close'], timeperiod=10)
+        x_k = []
+        close_k = []
+        fast_ema = []
+        slow_ema = []
+        for i in range(0, len(df)):
+            x_k.append(i)
+            close_k.append(df.iloc[i]["Close"])
+            fast_ema.append(df.iloc[i]["fast_ema"])
+            slow_ema.append(df.iloc[i]["slow_ema"])
+
+        plt.plot(x_k, close_k, label="close")
+        plt.plot(x_k, fast_ema, label="fast")
+        plt.plot(x_k, slow_ema, label="show")
+        plt.legend()
+        plt.show()
+
+
+if __name__ == "__main__":
+    request = HuobiSwapRequest("https://api.btcgateway.pro", "xxxx", "xxxx")
+    s = "BTC-USD"
+    p = "5min"
+    c = 200
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(MatPlot.get_data(s, p, c))
+    loop.close()
