3d13ba63f1c17f04af626fc5d453dc45c2628dd0
605355232@qq.com <dreams1234>
2020-05-29 14:39:21 +0800

    整理

--- coin.sh
+++ coin.sh
@@ -2,53 +2,47 @@ ACTION=$1
 CONFIG_PATH=$2
 CONFIG_FILE=${CONFIG_PATH##*/}
 MAIN_PATH=${CONFIG_FILE%%.*}
-SERVER_ID=${MAIN_PATH}server.pid
 OUT_FILE=${MAIN_PATH}.out
-if [ -f `$SERVER_ID` ]
-then
-    PROCESS_ID=`/bin/cat $SERVER_ID`
-    echo $PROCESS_ID
-fi
+
+COMMAND_STR="python main.py $CONFIG_PATH"
+ID=`ps -ef | grep "$COMMAND_STR" | grep -v "grep" | awk '{print $2}'`
+
 case $ACTION in
     start)
-    	if [ "$PROCESS_ID" ]
+    	if [ "$ID" ]
         then
-            echo "${CONFIG_PATH} is started. processId:${PROCESS_ID}"
+            echo "start fail. ${CONFIG_PATH} is started. processId:${ID}"
         else
             /usr/bin/nohup python main.py $CONFIG_PATH > $OUT_FILE &
-            echo $! > $SERVER_ID
-            PROCESS_ID=`/bin/cat $SERVER_ID`
-            if [ "$PROCESS_ID" ]
+            ID=$!
+            if [ "$ID" ]
             then
-                echo "${CONFIG_PATH} started success. processId:${PROCESS_ID}"
+                echo "${CONFIG_PATH} started success. processId:${ID}"
             else
                 echo "${CONFIG_PATH} start fail."
             fi          
     	fi
     ;;
     stop)
-        if [ "$PROCESS_ID" ]
+        if [ "$ID" ]
         then
-            kill $PROCESS_ID
-            /bin/rm -rf $SERVER_ID
+            kill $ID
             echo "${CONFIG_PATH} has killed."
         else
             echo "${CONFIG_PATH} is not running."
         fi
     ;;
     restart)
-        if [ "$PROCESS_ID" ]
+        if [ "$ID" ]
         then
             kill $PROCESS_ID
-            /bin/rm -rf $SERVER_id
             echo "${CONFIG_PATH} has killed."
         fi
         /usr/bin/nohup python main.py CONFIG_PATH > $OUT_FILE &
-        echo $! > $SERVER_ID
-        PROCESS_ID=`/bin/cat $SERVER_ID`
-        if [ "$PROCESS_ID" ]
+        ID=$!
+        if [ "$ID" ]
         then
-            echo "${CONFIG_PATH} started success. processId:$PROCESS_ID"
+            echo "${CONFIG_PATH} started success. processId:$ID"
         else
             echo "${CONFIG_PATH} start fail."
         fi    
