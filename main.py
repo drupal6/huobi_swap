320928f27e361afe0412e03461b0887c1e028d94
genelong <605355232@qq.com>
2020-05-30 18:53:33 +0800

    整理

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
     if len(sys.argv) > 1:
         config_file = sys.argv[1]
     else:
-        config_file = "config/eth-config.json"
+        config_file = "config/btc-config.json"
 
     quant.initialize(config_file)
     bs = initialize()
--- strategy/quantification_strategy.py
+++ strategy/quantification_strategy.py
@@ -189,7 +189,13 @@ class QuantificationStrategy(BaseStrategy):
                     self.short_status = 1
                     self.short_trade_size = self.short_position_weight[grid]
 
-
+    def show(self):
+        msg = super(QuantificationStrategy, self).show()
+        band_str = "".join(str(self.band))
+        band_size = str(len(self.band))
+        grid_str = str(self.grids[-1])
+        curr_price = str(self.band[self.grids[-1]])
+        return "%s\nband:%s\nsize:%s\ngrid:%s\nprice:%s" % (msg, band_str, band_size, grid_str, curr_price)
 
 
 
