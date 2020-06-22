060184aebd53cab08d8b8063c651959c6b5f2b9c
genelong <605355232@qq.com>
2020-06-23 00:03:56 +0800

    整理

---
+++ matploat/sigle_linear_regression_pic2.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from collections import deque
+from utils import sigle_linear_regression_util
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import copy
+import talib
+# 图形参数控制
+import pylab as pl
+import numpy as np
+from utils import fileutil
+from datetime import datetime
+from utils import tools
+import json
+from api.model.const import KILINE_PERIOD
+mpl.use('TkAgg')
+pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
+pd.set_option('display.max_rows', 1000)  # 最多显示行数.
+# pd.set_option('precision', 6)  # 浮点数的精度】
+pd.set_option('display.float_format', lambda x:'%.8f' % x)  # 设置不用科学计数法，保留两位小数.
+
+
+class MatPlot:
+    """https://blog.csdn.net/weixin_40014576/article/details/79918819"""
+    @classmethod
+    def get_data(cls):
+        pd_data = []
+        lines = fileutil.load_file("../../logs/eos-config.out")
+        columns_title = {"Date": 0, 'zero': 1, 'close': 2}
+        for line in lines:
+            if "change_curb:" in line:
+                strs = line.split("change_curb:")
+                date_str = strs[0].split("I [")[1].split(",")[0].rstrip().lstrip()
+                json_str = strs[1].rstrip().lstrip().replace("'", "\"")
+                data = json.loads(json_str)
+                append_data = {"Date": date_str, "zero": 0, "close": data["close"]}
+                add_columns = len(columns_title) == 3
+                for period in KILINE_PERIOD:
+                    append_data[period + "_ma"] = data[period + "_ma"]
+                    append_data[period + "_signal"] = data[period + "_signal"]
+                    if add_columns:
+                        columns_title[period + "_ma"] = len(columns_title)
+                        columns_title[period + "_signal"] = len(columns_title)
+                pd_data.append(append_data)
+        df = pd.DataFrame(pd_data, columns=columns_title)
+        df.set_index(["Date"], inplace=True)
+        MatPlot.show(df)
+
+    @classmethod
+    def show(cls, df):
+        df["signal1"] = df["1min_signal"] + df["5min_signal"] + df["15min_signal"]
+        df["ma1"] = df["1min_ma"] + df["5min_ma"] + df["15min_ma"]
+        df["signal2"] = df["1min_signal"] + df["5min_signal"] + df["15min_signal"] + df["30min_signal"]
+        df["ma2"] = df["1min_ma"] + df["5min_ma"] + df["15min_ma"] + df["30min_ma"]
+        close_values = df["close"]
+        signal_value1 = df["signal1"]
+        ma_values1 = df["ma1"]
+        signal_value2 = df["signal2"]
+        ma_values2 = df["ma2"]
+        zero_values = df["zero"]
+
+        # 设置画布，纵向排列的三个子图
+        fig, ax = plt.subplots(3, 1)
+
+        # 设置标签显示中文
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        plt.rcParams['axes.unicode_minus'] = False
+
+        # 调整子图的间距，hspace表示高(height)方向的间距
+        plt.subplots_adjust(hspace=.1)
+
+        # 设置第一子图的y轴信息及标题
+        ax[0].set_ylabel('Close price in ￥')
+        ax[0].set_title('A_Stock %s MACD Indicator' % ("test"))
+        close_values.plot(ax=ax[0], color='g', lw=1., legend=True, use_index=False)
+
+        # 应用同步缩放
+        ax[1] = plt.subplot(312, sharex=ax[0])
+        ma_values1.plot(ax=ax[1], color='y', lw=1., legend=True, sharex=ax[0], use_index=False)
+        signal_value1.plot(ax=ax[1], color='k', lw=1., legend=True, sharex=ax[0], use_index=False)
+        zero_values.plot(ax=ax[1], color='r', lw=1., legend=True, sharex=ax[0], use_index=False)
+
+        ax[2] = plt.subplot(313, sharex=ax[0])
+        ma_values2.plot(ax=ax[2], color='y', lw=1., legend=True, sharex=ax[0], use_index=False)
+        signal_value2.plot(ax=ax[2], color='k', lw=1., legend=True, sharex=ax[0], use_index=False)
+        zero_values.plot(ax=ax[2], color='r', lw=1., legend=True, sharex=ax[0], use_index=False)
+
+        # 设置间隔，以便图形横坐标可以正常显示（否则数据多了x轴会重叠）
+        scale = 100
+        interval = scale // 20
+        # 设置x轴参数，应用间隔设置
+        # 时间序列转换，(否则日期默认会显示时分秒数据00:00:00)
+        # x轴标签旋转便于显示
+        pl.xticks([i for i in range(1, scale + 1, interval)],
+                  [datetime.strftime(i, format='%Y-%m-%d') for i in
+                   pd.date_range(df.index[0], df.index[-1], freq='%dd' % (interval))],
+                  rotation=45)
+        plt.show()
+
+
+if __name__ == "__main__":
+    MatPlot.get_data()
