92518d683a920bf487e3ec03395f29b64cd34b32
605355232@qq.com <dreams1234>
2020-07-20 23:02:20 +0800

    整理网格

--- matploat/dichotomy_factor_pic.py
+++ matploat/dichotomy_factor_pic.py
@@ -1,13 +1,16 @@
 import pandas as pd
-from utils import fileutil
-import json
-from api.model.const import KILINE_PERIOD
+from api.huobi.huobi_request_swap import HuobiSwapRequest
+import asyncio
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 # 图形参数控制
 import pylab as pl
-from datetime import datetime
+import numpy as np
 from sklearn import svm
+from datetime import datetime
+import talib
+from collections import deque
+from utils import sigle_linear_regression_util
 mpl.use('TkAgg')
 pd.set_option('expand_frame_repr', False)  # 当列太多时不换行
 pd.set_option('display.max_rows', 1000)  # 最多显示行数.
@@ -25,109 +28,85 @@ class NodePoint:
 class MatPlot:
 
     @classmethod
-    def get_data(cls, profit, profit_period, size=500):
-        pd_data = []
-        lines = fileutil.load_file("../../logs/eth-config.out1")
-        columns_title = {"Date": 0, "close": 1, "cur_lead_dir": 2, "cur_price_dir": 3, "price_base": 4, "cur_cb_dir": 5,
-                         "cb_dir": 6, "cur_delay_dir": 7}
-        for line in lines:
-            if "IchimokuStrategy:" in line:
-                strs = line.split("IchimokuStrategy:")
-                date_str = strs[0].split("I [")[1].split(",")[0].rstrip().lstrip()
-                json_str = strs[1].rstrip().lstrip().replace("'", "\"")
-                append_data = {"Date": date_str}
-                data = json.loads(json_str)
-                append_data["close"] = data["close"]
-                append_data["cur_lead_dir"] = data["cur_lead_dir"]
-                append_data["cur_price_dir"] = data["cur_price_dir"]
-                append_data["price_base"] = data["price_base"]
-                append_data["cur_cb_dir"] = data["cur_cb_dir"]
-                append_data["cb_dir"] = data["cb_dir"]
-                append_data["cur_delay_dir"] = data["cur_delay_dir"]
-                pd_data.append(append_data)
-        df = pd.DataFrame(pd_data, columns=columns_title)
-        df.set_index(["Date"], inplace=True)
-        cls.handle_data(df, profit, profit_period, size)
+    async def get_data(cls, profit, profit_period, symbol, period="15min", size=500):
+        success, error = await request.get_klines(contract_type=symbol, period=period, size=1000)
+        if error:
+            print(error)
+            return None
+        if success:
+            data = success.get("data")
+            df = pd.DataFrame(data, columns={"id": 0, 'vol': 1, 'count': 2, 'open': 3, 'close': 4, 'low': 5,
+                                             'high': 6, 'amount': 7})
+            df = df[['id', 'open', 'high', 'low', 'close', 'vol']]
+            df = df.rename(
+                columns={"id": "Date", "open": "Open", "high": "High", "low": "Low", "close": "Close", "vol": "Volume"})
+            df["Leading"] = np.nan
+            df["k"] = np.nan
+            df["b"] = np.nan
+            df["c"] = np.nan
+            df["Date"] = pd.to_datetime(df["Date"], unit="s")
+            df.set_index(["Date"], inplace=True)
+
+            df_lenght = len(df)
+            period_1 = 10
+            y_d = deque(maxlen=period_1)
+            x_x = np.arange(1, period_1 + 1, 1)
+            for i in range(0, df_lenght):
+                if len(y_d) == period_1 and i < df_lenght:
+                    leading_y, k, b = sigle_linear_regression_util.leading_y(x_x, y_d)
+                    df.iloc[i, 5] = leading_y
+                    df.iloc[i, 6] = k
+                    df.iloc[i, 7] = b
+                y_d.append(df.iloc[i]["Close"])
+
+            cls.handle_data(df, profit, profit_period, period_1, size)
 
     @classmethod
-    def handle_data(cls, df, profit, profit_period, size):
+    def handle_data(cls, df, profit, profit_period, period_1, size):
         points = []
         for i in range(1, size - 1):
-            if df.iloc[i - 1]["close"] < df.iloc[i]["close"] and df.iloc[i]["close"] >= df.iloc[i + 1]["close"]:
-                points.append(NodePoint(i, df.iloc[i]["close"], 1))
-            if df.iloc[i - 1]["close"] > df.iloc[i]["close"] and df.iloc[i]["close"] <= df.iloc[i + 1]["close"]:
-                points.append(NodePoint(i, df.iloc[i]["close"], -1))
+            if df.iloc[i - 1]["Close"] < df.iloc[i]["Close"] and df.iloc[i]["Close"] >= df.iloc[i + 1]["Close"]:
+                points.append(NodePoint(i, df.iloc[i]["Close"], 1))
+            if df.iloc[i - 1]["Close"] > df.iloc[i]["Close"] and df.iloc[i]["Close"] <= df.iloc[i + 1]["Close"]:
+                points.append(NodePoint(i, df.iloc[i]["Close"], -1))
 
-        long_new_points = cls.calculate_points(points, profit, profit_period, 1)
-        short_new_points = cls.calculate_points(points, profit, profit_period, -1)
-        svm_model = cls.svn_train(df, long_new_points, size)
+        long_new_points = cls.calculate_points(df, points, profit, profit_period, 1)
+        short_new_points = cls.calculate_points(df, points, profit, profit_period, -1)
+        svm_model = cls.svn_train(df, period_1, long_new_points, size)
         cls.svn_predict(svm_model, df, long_new_points, size)
         buy_x, buy_y, sell_x, sell_y = cls.get_points(long_new_points)
         cls.show(df, buy_x, buy_y, sell_x, sell_y)
 
     @classmethod
-    def svn_train(cls, df, long_new_points, size):
+    def svn_train(cls, df, period_1, long_new_points, size):
         x_train = []  # 特征
         y_train = []  # 标记
+
         p_index = 0
         p1 = long_new_points[p_index]
         p2 = long_new_points[p_index + 1]
-        for i in range(0, size):
+        count = 0
+        for i in range(period_1, size):
             features = []
             cur_bar = df.iloc[i]
-            features.append(cur_bar["cur_lead_dir"])
-            features.append(cur_bar["cur_price_dir"])
-            features.append(cur_bar["price_base"])
-            features.append(cur_bar["cur_cb_dir"])
-            features.append(cur_bar["cb_dir"])
-            features.append(cur_bar["cur_delay_dir"])
+            close = float(cur_bar["k"])
+            leading = float(cur_bar["b"])
+            features.append(close - leading)
+
             if i > p2.x and p_index + 2 < len(long_new_points):
                 p_index = p_index + 2
                 p1 = long_new_points[p_index]
                 p2 = long_new_points[p_index + 1]
             if p_index + 2 > len(long_new_points):
                 break
+
             label = False
-            if p1.x <= i < p2.x:
+            if p1.x == i or p2.x == i:
                 label = True
+                count = count + 1
             x_train.append(features)
             y_train.append(label)
         svm_module = svm.SVC()
-        print(y_train)
-        svm_module.fit(x_train, y_train)
-        return svm_module
-
-    @classmethod
-    def svn_train(cls, df, long_new_points, size):
-        x_train = []  # 特征
-        y_train = []  # 标记
-        p_index = 0
-        p1 = long_new_points[p_index]
-        for i in range(0, size):
-            features = []
-            cur_bar = df.iloc[i]
-            features.append(cur_bar["cur_lead_dir"])
-            features.append(cur_bar["cur_price_dir"])
-            features.append(cur_bar["price_base"])
-            features.append(cur_bar["cur_cb_dir"])
-            features.append(cur_bar["cb_dir"])
-            features.append(cur_bar["cur_delay_dir"])
-            label = False
-            is_break = False
-            if p1.x == i:
-                label = True
-                p_index = p_index + 1
-                if p_index >= len(long_new_points):
-                    is_break = True
-                else:
-                    p1 = long_new_points[p_index]
-            x_train.append(features)
-            y_train.append(label)
-            if is_break:
-                break
-
-        svm_module = svm.SVC()
-        print(y_train)
         svm_module.fit(x_train, y_train)
         return svm_module
 
@@ -138,12 +117,9 @@ class MatPlot:
             x = []  # 特征
             features = []
             cur_bar = df.iloc[i]
-            features.append(cur_bar["cur_lead_dir"])
-            features.append(cur_bar["cur_price_dir"])
-            features.append(cur_bar["price_base"])
-            features.append(cur_bar["cur_cb_dir"])
-            features.append(cur_bar["cb_dir"])
-            features.append(cur_bar["cur_delay_dir"])
+            close = float(cur_bar["k"])
+            leading = float(cur_bar["b"])
+            features.append(close - leading)
             x.append(features)
             flag = svm_module.predict(x)
             if bool(flag):
@@ -151,20 +127,20 @@ class MatPlot:
                     last_flag = True
                 else:
                     if last_flag is False and long_new_points[-1].d == -1:
-                        long_new_points.append(NodePoint(i, cur_bar["close"], 1))
+                        long_new_points.append(NodePoint(i, cur_bar["Close"], 1))
                     last_flag = True
             elif bool(flag) is False:
                 if last_flag is None:
                     last_flag = False
                 else:
                     if last_flag is True and long_new_points[-1].d == 1:
-                        long_new_points.append(NodePoint(i, cur_bar["close"], -1))
+                        long_new_points.append(NodePoint(i, cur_bar["Close"], -1))
                     last_flag = False
             else:
                 last_flag = None
 
     @classmethod
-    def calculate_points(cls, points, profit, profit_period, dir):
+    def calculate_points(cls, df, points, profit, profit_period, dir):
         new_points = []
         i = 0
         l = len(points)
@@ -182,15 +158,17 @@ class MatPlot:
                     if dir == -1 and p1.d == 1 and p1.y < p2.y:
                         tmp_p1 = p2
                 else:
-                    if dir == 1 and p1.d == -1 and (p2.y - tmp_p1.y)/tmp_p1.y >= profit:
+                    if dir == 1 and p1.d == -1 and (p2.y - tmp_p1.y) / tmp_p1.y >= profit:
                         new_points.append(tmp_p1)
                         new_points.append(p2)
+                        print(df.iloc[tmp_p1.x].k, df.iloc[tmp_p1.x].b, df.iloc[p2.x].k, df.iloc[p2.x].b, 1)
                         add_point = True
                         i = i + j
                         break
-                    if dir == -1 and p1.d == 1 and (tmp_p1.y - p2.y)/tmp_p1.y >= profit:
+                    if dir == -1 and p1.d == 1 and (tmp_p1.y - p2.y) / tmp_p1.y >= profit:
                         new_points.append(tmp_p1)
                         new_points.append(p2)
+                        print(df.iloc[tmp_p1.x].k, df.iloc[tmp_p1.x].b, df.iloc[p2.x].k, df.iloc[p2.x].b, -1)
                         add_point = True
                         i = i + j
                         break
@@ -217,7 +195,10 @@ class MatPlot:
 
     @classmethod
     def show(cls, df, buy_x, buy_y, sell_x, sell_y):
-        price_values = df["close"]
+        price_values = df["Close"]
+        leading_values = df["Leading"]
+        high_values = df["High"]
+        low_values = df["Low"]
 
         fig, ax = plt.subplots(1, 1)
         plt.rcParams['font.sans-serif'] = ['SimHei']
@@ -228,6 +209,9 @@ class MatPlot:
         ax.set_ylabel('Close price in ￥')
         ax.set_title('A_Stock %s factor Indicator' % ("test"))
         price_values.plot(ax=ax, color='g', lw=1., legend=True, use_index=False)
+        leading_values.plot(ax=ax, color='r', lw=1., legend=True, use_index=False)
+        # high_values.plot(ax=ax, color='y', lw=1., legend=True, use_index=False)
+        # low_values.plot(ax=ax, color='y', lw=1., legend=True, use_index=False)
 
         plt.scatter(buy_x, buy_y, s=50, color='r', marker='^', alpha=0.5)
         plt.scatter(sell_x, sell_y, s=50, color='b', marker='^', alpha=0.5)
@@ -246,7 +230,12 @@ class MatPlot:
 
 
 if __name__ == "__main__":
-    c = 1000
+    request = HuobiSwapRequest("https://api.btcgateway.pro", "xxxx", "xxxx")
+    s = "BTC-USD"
+    p = "15min"
+    c = 300
     profit = 0.001
     profit_period = 10
-    MatPlot.get_data(profit, profit_period, c)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(MatPlot.get_data(profit, profit_period, s, p, c))
+    loop.close()
